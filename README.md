721a0fb6dc617d476da37dbf711f228ded6ad95a
Erick <erickxs78@gmail.com>
2020-08-03 23:51:13 -0400

    menu de notificaciones añadido

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Aplicaciones Web Avanzadas 1-2020
-## Big workers
+## Team - Big workers
 
-Este es la PWA desarrollada para la materia. Con la implementacion de Service Worker estatico y dinamico.
+Este es la PWA desarrollada para la materia. Con la implementacion de Service Worker
 
 To run this project :
 1. npm install
-2. npm run start
+2. npm run start    or  visit the url: https://educatin-2d309.web.app/ , here is the progresive web app deployed!!
--- public/js/reg_sw.js
+++ public/js/reg_sw.js
@@ -20,3 +20,93 @@ if ('serviceWorker' in navigator) {
     // happens when the app isn't served over a TLS connection (HTTPS)
     console.warn('Service Worker not available');
 }   
+
+
+// Notifications
+let notificationButton = document.getElementById('bNot');
+
+function confirmNotification(){
+  if ('serviceWorker' in navigator) {
+    let options = {
+      body: 'Servicio de Notificaciones Habilitado!',
+      icon: '../icons/favicon-32x32.png',
+      dir: 'ltr',
+      lang: 'es-ES', // BCP 47,
+      vibrate: [90, 30, 120],
+      badge: '/icons/favicon-32x32.png',
+      tag: 'confirm-notification',
+      renotify: true,
+      actions: [
+        { action: 'confirm', title: 'OK', icon: '/icons/favicon-32x32.png' },
+        { action: 'cancel', title: 'Dismiss', icon: '/icons/favicon-32x32.png' }
+      ]
+    };
+    navigator.serviceWorker.ready
+      .then(function(reg) {
+        reg.showNotification('Suscripción Habilitada!', options);
+    })        
+  }
+}
+
+function subscribePush(){
+  let reg2;
+  
+  if (!('serviceWorker' in navigator)) {
+    return;
+  }
+
+  navigator.serviceWorker.ready
+    .then(function(reg) {
+      reg2 = reg;
+      return reg.pushManager.getSubscription()
+    })
+    .then(function(subscription) {
+      if (subscription === null) {
+        // New subscription
+        let vapidPublicKey = 'BMMUuzeFVpdf51gEwFMLF3Ndl1dtZXfaeJrU2OkfP24zUBkHONtQO16gX571Q3Oz9pNLU5kPXsZfUIjNGMAswhk'
+        let convertedVapidPublicKey = urlBase64ToUint8Array(vapidPublicKey)
+        return reg2.pushManager.subscribe({
+          userVisibleOnly: true ,
+          applicationServerKey: convertedVapidPublicKey
+        })
+    
+      } else {
+        // already subscribed
+      }
+    })
+    .then(function(subscription) {
+      return fetch('https://primera-api-rest.herokuapp.com/suscription', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify(subscription)
+      })
+    })
+    .then(function(res) {
+      if (res.ok) {
+        confirmNotification()
+      }
+    })
+    .catch(function(err) {
+      console.log(err)
+    });  
+}
+
+function notificationPermit(){
+  Notification.requestPermission(function(result){
+    if (result !== 'granted'){
+      console.log("Notification permit: REJECTED")
+    } else{ 
+        console.log("Notification permit: ", result)
+        subscribePush()
+    }
+  })
+}
+
+if ('Notification' in window && 'serviceWorker' in navigator){
+  notificationButton.style.display = 'inline-block'
+  notificationButton.addEventListener('click', notificationPermit)
+}
+// end notifications
